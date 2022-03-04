b194b76112a131ec935ba17c0a420a10e4f2979b
Daniel Estehghari <danielestehghari@Daniels-MacBook-Pro.local>
2022-03-04 22:28:27 +0100

    restructure

--- cleanup/functions.js
+++ cleanup/functions.js
@@ -1,140 +1,120 @@
 
 
-const {Viewer, SimpleMarker, geodeticToEnu, enuToGeodetic} = mapillary;
+const { Viewer, SimpleMarker, geodeticToEnu, enuToGeodetic } = mapillary;
 
-let audioCtx;
-let panner;
-let track;
-let listener;
-let viewerPos;
-let audioPos;
-let markerComponent;
-let refLat = 55.680763888889
-let refLng = 12.560666666667
+const exports = {
+    consts: {
+        refLat: 55.680763888889,
+        refLng: 12.560666666667,
+
+        // These variables define our destination coordinates
+        targetLat: 55.680208333332985, 
+        targetLng: 12.556296296296068
+    },
+    funcs: {
+        initialize() {
+            // Initialize viewer
+            // Parameters for the Mapillary Viewer constructor 
+            let viewer = new Viewer({
+                accessToken: 'MLY|5055210414499610|cc32fb072365a29201fee81cf2d9e241',
+                container: 'mly', // the ID of our container defined in the HTML body
+                imageId: '2921734351448761',
+                //imageId: '2556983327944830',
+                component: { marker: true, cover: false }
+            });
+        
+            viewer.setFilter(["==", "cameraType", 'spherical'], ["==", "creatorId", "gisfrb"]);
+
+            // Initialize Audio
+            const audioCtx = new AudioContext();
+            const audioElement = document.getElementById('music');
+            const track = audioCtx.createMediaElementSource(audioElement);
+            const listener = audioCtx.listener;
+
+            const panner = audioCtx.createPanner();
+            panner.panningModel = 'HRTF';
+            panner.distanceModel = 'inverse';
+            panner.refDistance = 1;
+            panner.maxDistance = 10000;
+            panner.rolloffFactor = 1;
+            panner.coneInnerAngle = 360;
+            panner.coneOuterAngle = 0;
+            panner.coneOuterGain = 0;
+        
+            // TODO: add filter and reverb
+
+            track.connect(panner);
+            panner.connect(audioCtx.destination);
+
+            return { viewer, panner, audioCtx, audioElement, track, listener };
+        },
+        setMarker(viewer, latitude, longitude, id) {
+            viewer.activateComponent("marker");
+            let markerComponent = viewer.getComponent("marker");
+            
+            markerComponent.add([
+                new SimpleMarker(id, { lat: latitude, lng: longitude })
+            ]);
+        },
+        
+        //  Haversine formula to calculate the distance between two points
+        distance(lat1, lon1, lat2, lon2) {
+            const p = Math.PI / 180
+            const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 + 
+                    Math.cos(lat1 * p) * Math.cos(lat2 * p) * 
+                    (1 - Math.cos((lon2 - lon1) * p)) / 2;
+          
+            return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+        },
+        
+        
+        //AUDIO
+        
+        //  Function to create a new audiocontext, panner and listener for the audio and select audio source. 
+        //  The panner controls the audio position (only updated once) and the listener tells the panner about the users location so it can 
+        //  modulate the signal accordingly.
+        
+        
+        //  Sets the listener position. geoDeticToEnu converts latitude and longitude to x-y-z coordinates in a local topocentric plane 
+        //  using a reference latitude and longitude.
+        setListenerPos(listener, audioCtx, latitude, longitude) {
+            const pos = geodeticToEnu(longitude, latitude, 0, exports.consts.refLng, exports.consts.refLat, 0);
+            listener.positionX.setValueAtTime(pos[0], audioCtx.currentTime);
+            listener.positionY.setValueAtTime(pos[1], audioCtx.currentTime);
+            listener.positionZ.setValueAtTime(0, audioCtx.currentTime);  
+        },
+        // Sets listener point of view using the bearing (relative compass angle to north).
+        setListenerPov(listener, audioCtx, viewer, bearing) {
+            const x = listener.positionX.value + Math.sin(bearing);
+            const y = listener.positionY.value + Math.cos(bearing);
+            console.log(x);
+            console.log(y);
+             
+            listener.forwardX.setValueAtTime(x, audioCtx.currentTime);
+            listener.forwardY.setValueAtTime(y, audioCtx.currentTime);
+            listener.forwardZ.setValueAtTime(0, audioCtx.currentTime);
+            listener.upX.setValueAtTime(0, audioCtx.currentTime);
+            listener.upY.setValueAtTime(0, audioCtx.currentTime);
+            listener.upZ.setValueAtTime(0, audioCtx.currentTime);
+            
+            const coords = enuToGeodetic(x, y, 0, exports.consts.refLng, exports.consts.refLat, 0);
+            
+            let markerComponent = viewer.getComponent("marker");
+            markerComponent.add([
+                new SimpleMarker("bearing", {lat: coords[1], lng: coords[0]})
+            ]);
+            //filter.frequency.value = event.bearing * (hipass_freq/360)          
+        },
+        setPannerPos(panner, audioCtx, longitude, latitude) {
+            const pos = geodeticToEnu(longitude, latitude, 0, exports.consts.refLng, exports.consts.refLat, 0);
+            panner.positionX.setValueAtTime(pos[0], audioCtx.currentTime);
+            panner.positionY.setValueAtTime(pos[1], audioCtx.currentTime);
+            panner.positionZ.setValueAtTime(0, audioCtx.currentTime);
+        }
+    }
+};
+
 
 //MAPILLARY
 
-//  Viewer constructor
-function initializeViewer(params){
-    let viewer = new Viewer(params)
-    viewer.setFilter(["==", "cameraType", 'spherical'], ["==", "creatorId", "gisfrb"]);
-    return viewer;
-
-}
-
-
-function setMarker(latitude, longitude, id){
-    markerComponent = viewer.getComponent("marker");
-    viewer.activateComponent("marker");
-    let targetMarker = new SimpleMarker(id, {lat: latitude, lng: longitude});
-    markerComponent.add([targetMarker]);
-
-}
-
-//  Haversine formula to calculate the distance between two points
-function distance(lat1, lon1, lat2, lon2) {
-    var p = 0.017453292519943295;    // Math.PI / 180
-    var c = Math.cos;
-    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
-            c(lat1 * p) * c(lat2 * p) * 
-            (1 - c((lon2 - lon1) * p))/2;
-  
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-  }
-
-
-//AUDIO
-
-//  Function to create a new audiocontext, panner and listener for the audio and select audio source. 
-//  The panner controls the audio position (only updated once) and the listener tells the panner about the users location so it can 
-//  modulate the signal accordingly.
-
-function initializeAudio(filter = false, reverb = false){
-    audioCtx = new AudioContext();
-    panner = audioCtx.createPanner();
-    audioElement = document.getElementById('music');
-    track = audioCtx.createMediaElementSource(audioElement);
-    listener = audioCtx.listener;
-   
-    panner.panningModel = 'HRTF';
-    panner.distanceModel = 'inverse';
-    panner.refDistance = 1;
-    panner.maxDistance = 10000;
-    panner.rolloffFactor = 1;
-    panner.coneInnerAngle = 360;
-    panner.coneOuterAngle = 0;
-    panner.coneOuterGain = 0;
-
-    if (filter){
-        console.log("Filter not yet implemented.")
-    }
-
-    if (reverb){
-        console.log("Reverb not yet implemented.")
-    }
-
-    track.connect(panner);
-    panner.connect(audioCtx.destination);
-
-}
-//  Sets the listener position. geoDeticToEnu converts latitude and longitude to x-y-z coordinates in a local topocentric plane 
-//  using a reference latitude and longitude.
-function setListenerPos(latitude, longitude){
-
-        pos = geodeticToEnu(longitude, latitude, 0, refLng, refLat, 0);
-        listener.positionX.setValueAtTime(pos[0], audioCtx.currentTime);
-        listener.positionY.setValueAtTime(pos[1], audioCtx.currentTime);
-        listener.positionZ.setValueAtTime(0, audioCtx.currentTime);
-        
-    }
-// Sets listener point of view using the bearing (relative compass angle to north).
-function setListenerPov(bearing){
-    x = listener.positionX.value + Math.sin(bearing);
-    y = listener.positionY.value + Math.cos(bearing);
-    console.log(x);
-    console.log(y);
-    listener.forwardX.setValueAtTime(x, audioCtx.currentTime);
-    listener.forwardY.setValueAtTime(y, audioCtx.currentTime);
-    listener.forwardZ.setValueAtTime(0, audioCtx.currentTime);
-    listener.upX.setValueAtTime(0, audioCtx.currentTime);
-    listener.upY.setValueAtTime(0, audioCtx.currentTime);
-    listener.upZ.setValueAtTime(0, audioCtx.currentTime);
-    coords = enuToGeodetic(x, y, 0, refLng, refLat, 0);
-    let bearingmarker = new SimpleMarker("bearing", {lat: coords[1], lng: coords[0]});
-    markerComponent.add([bearingmarker]);
-    //filter.frequency.value = event.bearing * (hipass_freq/360)
-  
-}
-
-function setPannerPos(longitude, latitude){
-    pos = geodeticToEnu(longitude, latitude, 0, refLng, refLat, 0);
-    panner.positionX.setValueAtTime(pos[0], audioCtx.currentTime);
-    panner.positionY.setValueAtTime(pos[1], audioCtx.currentTime);
-    panner.positionZ.setValueAtTime(0, audioCtx.currentTime);
-
-}
-function playPause(){
-    if (audioElement.paused || audioElement.currentTime == 0){
-        audioElement.play().then(function() {
-            console.log("Playback started!");
-          }).catch(function(error) {
-            console.log("Playback failed");
-            // Show a UI element to let the user manually start playback.
-          });
-        
-    } else {
-      audioElement.pause().then(function(){
-          console.log("Playback paused!");
-      });
-      }
-    }
-
-function resumeIfSuspended(){
-    if (audioCtx.state == "suspended") {
-        audioCtx
-            .resume()
-            .then(() => console.log("Resumed"))
-            .catch(() => console.error("Could not resume"));
-    } 
-}
-//OPENROUTESERVICE – Only used for
+window.exports = exports;
--- cleanup/main.js
+++ cleanup/main.js
@@ -1,45 +1,47 @@
-let targetLat = 55.680208333332985 //These variables define our destination coordinates
-let targetLng = 12.556296296296068
 
+(function() {
 
+    // Initialize the viewer (calls the constructor in functions.js)
+    const E = window.exports;
+    let { viewer, panner, audioCtx, audioElement, track, listener } = E.funcs.initialize();
+    E.funcs.setMarker(viewer, E.consts.targetLat, E.consts.targetLng, "target");
+    
+    // This tells the audio module where to position the viewer
+    viewer.getPosition().then(pos => E.funcs.setListenerPos(listener, audioCtx, pos.lat, pos.lng));
+    E.funcs.setPannerPos(panner, audioCtx, E.consts.targetLng, E.consts.targetLat);
+    
+    const resumeIfSuspended = () => {
+        if (audioCtx.state === "suspended") {
+            audioCtx.resume()
+                .then(() => console.log("Resumed"))
+                .catch(() => console.error("Could not resume"));
+        }
+    };
+    
+    // Update listener position if user updates their position
+    viewer.on('position', event => {
+        resumeIfSuspended();
+        viewer.getPosition()
+            .then(position => E.funcs.setListenerPos(listener, audioCtx, position.lat, position.lng));
+    });
 
-//Parameters for the Mapillary Viewer constructor 
-let options = {
-    accessToken: 'MLY|5055210414499610|cc32fb072365a29201fee81cf2d9e241',
-    container: 'mly', // the ID of our container defined in the HTML body
-    imageId: '2921734351448761',
-    //imageId: '2556983327944830',
-    component: { marker: true, cover: false }
-  };
-
-//Initialize the viewer (calls the constructor in functions.js)
-let viewer = initializeViewer(options);
-setMarker(targetLat, targetLng, "target");
-
-
-initializeAudio();
-viewer.getPosition().then(pos => setListenerPos(pos.lat, pos.lng)); //This tells the audio module where to position the viewer
-setPannerPos(targetLng, targetLat)
-
-
-//Update listener position if user updates their position
-viewer.on('position', async (event) => {
-  let position =  viewer.getPosition();
-  resumeIfSuspended();
-  position.then((position)=>{
-      setListenerPos(position.lat, position.lng)
-      
-    })
-
-});
-//Update listener POV if user updates their POV 
-viewer.on('bearing', (event) => {
-  bearing = event.bearing * (Math.PI/180) //get bearing and convert to radians
-  resumeIfSuspended();
-  setListenerPov(bearing) //placeaudio
-
-});
-
-
-document.getElementById("start").onclick = function() {playPause()};
+    // Update listener POV if user updates their POV 
+    viewer.on('bearing', event => {
+        bearing = event.bearing * (Math.PI / 180) // Get bearing and convert to radians
+        resumeIfSuspended();
+        E.funcs.setListenerPov(listener, audioCtx, viewer, bearing);
+    });
+    
+    document.getElementById("start").onclick = event => {
+        if (audioElement.paused || !audioElement.currentTime) {
+            audioElement.play()
+                .then(() => console.log("Playback started!"))
+                .catch(e => console.error("Playback failed"));
+        } else {
+            audioElement.pause()
+                .then(() => console.log("Playback paused!"))
+                .catch(e => console.error("Pause failed"));
+        }
+    };
+})();
 
