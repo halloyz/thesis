5539320272996e892017f32d598d53b3eede40d0
Daniel Estehghari <danielestehghari@Daniels-MacBook-Pro.local>
2022-03-06 17:25:46 +0100

    added calcAngle2 and setPannerPos2

--- cleanup/functions.js
+++ cleanup/functions.js
@@ -18,8 +18,9 @@ const exports = {
             let viewer = new Viewer({
                 accessToken: 'MLY|5055210414499610|cc32fb072365a29201fee81cf2d9e241',
                 container: 'mly', // the ID of our container defined in the HTML body
-                imageId: '2921734351448761',
+                //imageId: '2921734351448761', //starting point
                 //imageId: '2556983327944830',
+                imageId: '132246192214397',
                 component: { marker: true, cover: false }
             });
         
@@ -33,20 +34,28 @@ const exports = {
 
             const panner = audioCtx.createPanner();
             panner.panningModel = 'HRTF';
-            panner.distanceModel = 'inverse';
+            panner.distanceModel = 'linear';
             panner.refDistance = 1;
             panner.maxDistance = 10000;
-            panner.rolloffFactor = 1;
-            panner.coneInnerAngle = 360;
-            panner.coneOuterAngle = 0;
+            panner.rolloffFactor = 10;
+            panner.coneInnerAngle = 0;
+            panner.coneOuterAngle = 360;
             panner.coneOuterGain = 0;
+
+            const filter = audioCtx.createBiquadFilter();
+            filter.type = "hipass"
+            const hipass_freq = 20000;
+            filter.frequency.value = hipass_freq;
+
+            const stereoPanner = audioCtx.createStereoPanner();
         
             // TODO: add filter and reverb
 
-            track.connect(panner);
-            panner.connect(audioCtx.destination);
+            track.connect(stereoPanner);
+            stereoPanner.connect(filter);
+            filter.connect(audioCtx.destination)
 
-            return { viewer, panner, audioCtx, audioElement, track, listener };
+            return { viewer, panner, stereoPanner, filter, audioCtx, audioElement, track, listener };
         },
         setMarker(viewer, latitude, longitude, id) {
             viewer.activateComponent("marker");
@@ -87,9 +96,9 @@ const exports = {
         setListenerPov(listener, audioCtx, viewer, bearing) {
             const x = listener.positionX.value + Math.sin(bearing);
             const y = listener.positionY.value + Math.cos(bearing);
-            console.log(x);
-            console.log(y);
-             
+
+            //console.log(x);
+            //console.log(y);
             listener.forwardX.setValueAtTime(x, audioCtx.currentTime);
             listener.forwardY.setValueAtTime(y, audioCtx.currentTime);
             listener.forwardZ.setValueAtTime(0, audioCtx.currentTime);
@@ -110,7 +119,96 @@ const exports = {
             panner.positionX.setValueAtTime(pos[0], audioCtx.currentTime);
             panner.positionY.setValueAtTime(pos[1], audioCtx.currentTime);
             panner.positionZ.setValueAtTime(0, audioCtx.currentTime);
+        },
+
+        setStereoPannerPos(stereoPanner, audioCtx, angle){
+            //angle = Math.min(135,Math.max(-135,angle))
+            angle = Math.sin(angle*(Math.PI/180))
+            //angle = (angle > 0) ? sin(angle+)
+            //stereoPanner.pan.setValueAtTime((angle/-135), audioCtx.currentTime);
+            //console.log(angle)
+            stereoPanner.pan.setValueAtTime((angle), audioCtx.currentTime);
+
+        
+        },
+
+        setStereoPannerPos2(stereoPanner, audioCtx, angle){
+            let val;
+            if ((150 <= angle && angle < 180) || (-150 >= angle && angle > -180)){
+                val = Math.sin(2*angle*Math.PI/180);
+            }
+            else{
+                val = -Math.sin(angle*Math.PI/180);
+           }
+            console.log(val)
+            stereoPanner.pan.setValueAtTime(val, audioCtx.currentTime);
+            //console.log(val)
+            //stereoPanner.pan.setValueAtTime((angle), audioCtx.currentTime);
+
+        
+        },
+
+        setFilterCutoff(filter, audioCtx, angle){
+            let s = (500 - 20000) / (180 - 90)
+            let input = Math.abs(angle)
+            if (input > 90){
+                let output = 20000 + s * (input-90)
+                console.log(output);
+                filter.frequency.setValueAtTime(output, audioCtx.currentTime)
+            }
+
+        },
+        calcAngle(listener, bearing, pannerLongitude, pannerLatitude) {
+            const pos = geodeticToEnu(
+                pannerLongitude, pannerLatitude, 0,
+                exports.consts.refLng, exports.consts.refLat, 0
+            );
+            
+            const pCoords = { x: pos[0], y: pos[1] };
+            const lCoords = { x: listener.positionX.value, y: listener.positionY.value };
+            const dirCoords = { 
+                x: listener.positionX.value + Math.sin(bearing),
+                y: listener.positionY.value + Math.cos(bearing)
+            };
+
+            const ab = { x: lCoords.x - pCoords.x, y: lCoords.y - pCoords.y };
+            const cb = { x: lCoords.x - dirCoords.x, y: lCoords.y - dirCoords.y };
+
+            const dot = ab.x * cb.x + ab.y * cb.y;   // dot product
+            const cross = ab.x * cb.y - ab.y * cb.x; // cross product
+            const alpha = Math.atan2(cross, dot);
+            
+            return Math.floor(alpha * 180.0 / Math.PI + 0.5);
+        },
+
+        calcAngle2(listener, bearing, pannerLongitude, pannerLatitude) {
+            const pos = geodeticToEnu(
+                pannerLongitude, pannerLatitude, 0,
+                exports.consts.refLng, exports.consts.refLat, 0
+            );
+            
+            const pCoords = { x: pos[0], y: pos[1] };
+            const lCoords = { x: listener.positionX.value, y: listener.positionY.value };
+            const dirCoords = { 
+                x: listener.positionX.value + Math.sin(bearing),
+                y: listener.positionY.value + Math.cos(bearing)
+            };
+
+            const lp = { x: pCoords.x - lCoords.x, y: pCoords.y - lCoords.y };
+            const ld = { x: dirCoords.x - lCoords.x, y: dirCoords.y - lCoords.y };
+            //const lp_ = Math.sqrt((pCoords.x - lCoords.x)*((pCoords.x - lCoords.x)+(pCoords.y-lCoords.y)*(pCoords.y-lCoords.y))
+            //const ld_ = Math.sqrt((dirCoords.x-lCoords.x)*(dirCoords.x-lCoords.x)+(dirCoords.y-lCoords.y)*(dirCoords.y-lCoords.y))
+            const lp_ = Math.sqrt((lp.x*lp.x+lp.y*lp.y));
+            const ld_ = Math.sqrt((ld.x*ld.x+ld.y*ld.y));
+            const det = lp.x * ld.y - lp.y * ld.x;
+            const dot = lp.x*ld.x+lp.y*ld.y;
+            //const cos_alpha = (lp.x*ld.x+lp.y*ld.y)/(lp_*ld_)
+            let alpha = Math.atan2(det, dot);
+            alpha = alpha * (180.0/Math.PI);
+            //console.log(alpha)
+            return alpha;
         }
+        
     }
 };
 
--- cleanup/main.js
+++ cleanup/main.js
@@ -1,14 +1,16 @@
 
 (function() {
 
+})();
+
     // Initialize the viewer (calls the constructor in functions.js)
     const E = window.exports;
-    let { viewer, panner, audioCtx, audioElement, track, listener } = E.funcs.initialize();
+    let { viewer, panner, filter, stereoPanner, audioCtx, audioElement, track, listener } = E.funcs.initialize();
     E.funcs.setMarker(viewer, E.consts.targetLat, E.consts.targetLng, "target");
     
     // This tells the audio module where to position the viewer
     viewer.getPosition().then(pos => E.funcs.setListenerPos(listener, audioCtx, pos.lat, pos.lng));
-    E.funcs.setPannerPos(panner, audioCtx, E.consts.targetLng, E.consts.targetLat);
+    //E.funcs.setPannerPos(panner, audioCtx, E.consts.targetLng, E.consts.targetLat);
     
     const resumeIfSuspended = () => {
         if (audioCtx.state === "suspended") {
@@ -27,9 +29,14 @@
 
     // Update listener POV if user updates their POV 
     viewer.on('bearing', event => {
-        bearing = event.bearing * (Math.PI / 180) // Get bearing and convert to radians
+        const bearing = event.bearing * (Math.PI / 180) // Get bearing and convert to radians
         resumeIfSuspended();
-        E.funcs.setListenerPov(listener, audioCtx, viewer, bearing);
+        //E.funcs.setListenerPov(listener, audioCtx, viewer, bearing);
+        
+        const angle = E.funcs.calcAngle2(listener, bearing, E.consts.targetLng, E.consts.targetLat);
+        //console.log(`The angle is: ${angle}`);
+        E.funcs.setStereoPannerPos2(stereoPanner, audioCtx, angle);
+        //E.funcs.setFilterCutoff(filter, audioCtx,angle);
     });
     
     document.getElementById("start").onclick = event => {
@@ -43,5 +50,3 @@
                 .catch(e => console.error("Pause failed"));
         }
     };
-})();
-
