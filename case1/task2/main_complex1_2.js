d691e0e34f43ac54b4c44b2ca34dc4d3146a40c1
Daniel Estehghari <danielestehghari@Daniels-Mac-mini.local>
2022-04-12 23:36:20 +0200

    references

--- case1/task2/main_complex1_2.js
+++ case1/task2/main_complex1_2.js
@@ -2,7 +2,7 @@ let startTime = 0
 let endTime = 0
 let result = window.localStorage.getItem(("result"));
 result = JSON.parse(result)
-
+window.location.href = "/finish.html"
 let current = 0; //Keeps track of current checkpoint index, (initial: 0)
 const threshold = 20; //Distance in m for when audio should move to next checkpoint (default: 20)
 let posMarker; //Will show user position on map
