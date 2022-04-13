ab884f7bdf4a846c17365de62cef7a1d4428359a
Daniel Estehghari <danielestehghari@Daniels-Mac-mini.local>
2022-04-14 01:01:09 +0200

    new site

---
+++ cases.js
@@ -0,0 +1,44 @@
+const A = { imageId: '1552132681648763', targetLat: 55.678210776097494, targetLng: 12.556136444185778 } //Route A
+const B = { imageId: '973113553478933', targetLat: 55.68171833505691, targetLng: 12.551868533141779 } //Route B
+const C = { imageId: '138782904971107', targetLat: 55.683345399022, targetLng: 12.548227539056} //Route C
+
+const coords = {
+    case1:
+    {
+        task1: A,
+        task2: B,
+        task3: C,
+    },
+    case2:
+    {
+        task1: A,
+        task2: C,
+        task3: B,
+    },
+    case3:
+    {
+        task1: B,
+        task2: A,
+        task3: C,
+    },
+    case4:
+    {
+        task1: B,
+        task2: C,
+        task3: A,
+    },
+    case5:
+    {
+        task1: C,
+        task2: A,
+        task3: B,
+    },
+    case6:
+    {
+        task1: C,
+        task2: B,
+        task3: A,
+    },
+    
+}
+window.cases = coords;
---
+++ lander.js
@@ -0,0 +1,24 @@
+function getRandomKey(obj) {
+  const keys = Object.keys(obj);
+
+  //return obj[keys[ keys.length * Math.random() << 0]];
+  return keys[Math.floor(Math.random() * keys.length)];
+}
+
+// Upon clicking the link, the user will be randomly given a number 1-6, corresponding to which route will be given to them for each task.
+//const cases = window.cases;
+const usercase = getRandomKey(cases);
+
+let result = {case: usercase, email: null, task1: null}
+
+document.getElementById("proceed").onclick = function(){
+    result.email = document.getElementById("email").value
+    console.log(result)
+    if (!result.email){
+      alert("Please enter your e-mail")
+    }
+    else{
+    window.localStorage.setItem("result", JSON.stringify(result));
+    window.location.href = "task1/instructions.html";
+    }
+}
