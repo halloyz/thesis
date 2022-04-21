9e23f46ca486cf2ccfb45968c7a26f1828788799
Daniel Estehghari <danielestehghari@Daniels-Mac-mini.local>
2022-04-21 16:23:57 +0200

    changed instructions

--- finish.js
+++ finish.js
@@ -1,5 +1,5 @@
 let result = window.localStorage.getItem(("result"));
-
+result = JSON.parse(result);
 function download(content, filename){
     const a = document.createElement('a') // Create "a" element
     const blob = new Blob([JSON.stringify(content)], {type: 'application/json'}) // Create a blob (file-like object)
